f7c238b59cd470d7f8266c1c4c1a10a534339196
Dobsgw <george.w.dobson@gmail.com>
2021-06-28 13:14:57 +0300

    Revert "sm8250-common: Adapt to Fluid"
    This reverts commit 58d8259a43c5516f4a5663245310727eca15437b.

--- kona.mk
+++ kona.mk
@@ -204,7 +204,7 @@ PRODUCT_PACKAGES += \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 PRODUCT_COPY_FILES += \
-    vendor/fluid/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 endif
 
 PRODUCT_PACKAGES += \
