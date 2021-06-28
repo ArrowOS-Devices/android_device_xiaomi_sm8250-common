6ba656c1eb2ce4c4afcdbdca6236d8d6f4031bf5
Dobsgw <george.w.dobson@gmail.com>
2021-06-28 13:23:35 +0300

    Revert "sm8250-common: Build default Trust HIDL"
    This reverts commit e46be1397259682bdd1b4bfd5e74c4e0dbcf8c07.

--- kona.mk
+++ kona.mk
@@ -470,10 +470,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.touchfeature@1.0.vendor
 
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.2-service-qti
