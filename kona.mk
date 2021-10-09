0523a19e42068d14718c9be86e8a6d377a91743c
yukiscape <garikapatiamartya@gmail.com>
2021-10-09 12:22:36 +0200

    sm8250-common: Add hardware/qcom-caf/wlan to namespaces and wpa_supplicant_8_lib to cfi path

--- kona.mk
+++ kona.mk
@@ -447,7 +447,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi
+    hardware/xiaomi \
+    hardware/qcom-caf/wlan \
 
 # Telephony
 PRODUCT_PACKAGES += \
@@ -520,6 +521,10 @@ PRODUCT_PACKAGES += \
 
 #PRODUCT_BOOT_JARS += \
 #    WfdCommon
+
+# Wlan
+PRODUCT_CFI_INCLUDE_PATHS += \
+    hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sm8250-common/sm8250-common-vendor.mk)
