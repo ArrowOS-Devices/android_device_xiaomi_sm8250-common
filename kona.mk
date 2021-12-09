6766b665bab1aa08da41e1967ddd05c07dcfbb77
Dobsgw <38656887+Dobsgw@users.noreply.github.com>
2021-12-09 15:06:47 +0300

    sm8250-common: build 32 bit audio only
    Our devices do not use 64 bit audio so build 32 bit only.

--- kona.mk
+++ kona.mk
@@ -137,8 +137,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@6.0-impl:32 \
+    android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.soundtrigger@2.3-impl
