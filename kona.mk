2ed72d2faefa18d26a9128d9a30870a988bd06bc
Dobsgw <38656887+Dobsgw@users.noreply.github.com>
2022-03-28 14:28:20 +0300

    sm8250-common: camera: snap -> gcam go
    Ship with Gcam Go in favour of snap as it's much better

--- kona.mk
+++ kona.mk
@@ -156,6 +156,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+PRODUCT_PACKAGES += \
+    GCamGOPrebuilt-V2
+
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
