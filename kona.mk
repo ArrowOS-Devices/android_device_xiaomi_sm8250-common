e68c5347e700cdd30367f84789986f449046412b
Dobsgw <38656887+Dobsgw@users.noreply.github.com>
2022-07-28 20:29:39 +0100

    sm8250-common: fix typo from upstream
    Bruh moment resolved

--- kona.mk
+++ kona.mk
@@ -399,7 +399,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-arrow
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
