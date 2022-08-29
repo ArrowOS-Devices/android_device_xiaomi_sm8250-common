e5689363b560f4ac8517e45b37bb3a25fa574673
Dobsgw <george.w.dobson@gmail.com>
2022-08-29 09:34:30 +0100

    sm8250-common: uprev audio hal 7.0 > 7.1

--- kona.mk
+++ kona.mk
@@ -116,7 +116,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
-    android.hardware.audio@7.0-impl:32 \
+    android.hardware.audio@7.1-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.audio.service \
