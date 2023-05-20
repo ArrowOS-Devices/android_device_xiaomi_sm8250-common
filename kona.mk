89fcaabf3a01b18bbb793f44e487e7e4f5ec2463
Dobsgw <george.w.dobson@gmail.com>
2023-05-20 08:20:18 +0100

    Revert "sm8250-common: Rename libperfmgr to xiaomi-sm8250-libperfmgr"
    This reverts commit 966c24550680335bcc488be7fe94e97a74f6dc1c.

--- kona.mk
+++ kona.mk
@@ -371,7 +371,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-sm8250-libperfmgr
+    android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
