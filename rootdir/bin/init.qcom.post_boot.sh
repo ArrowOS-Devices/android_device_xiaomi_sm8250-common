8825c8e46b716ff31f4535305a8b3366c0f28cde
madmax7896 <madmax78968@gmail.com>
2023-07-07 20:16:39 +0530

    Revert "sm8250-common: Do not touch sleep disabled"
    This reverts commit 98b40ce189115ce5f69c22843622b37a02f83bc7.

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -5868,6 +5868,7 @@ case "$target" in
         # device/target specific folder
         setprop vendor.dcvs.prop 0
 	setprop vendor.dcvs.prop 1
+    echo N > /sys/module/lpm_levels/parameters/sleep_disabled
     configure_memory_parameters
     ;;
 esac
